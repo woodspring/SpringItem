6b4227fe77ffa7b133415236e2cb4e36ce46da6f
woodspring <woodspring.toronto@gmail.com>
2021-08-25 00:14:06 -0400

    first commit

---
+++ src/main/java/woodspring/springitem/SpringItemApplication.java
@@ -0,0 +1,13 @@
+package woodspring.springitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringItemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woodspring/springitem/SpringItemApplicationTests.java
@@ -0,0 +1,13 @@
+package woodspring.springitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
